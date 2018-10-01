35997dd78eca64abce26ed3f06457e89c648d31a
Patrick Naughton <patricknaughton01@gmail.com>
2018-10-01 10:06:50 -0500

    Added some error checks to javascriptrequests.php

--- javascriptrequests.php
+++ javascriptrequests.php
@@ -3,9 +3,14 @@
     header("Content-type: text/json");
     $_POST = json_decode((string) file_get_contents("php://input"), true);
     if(isset($_POST["action"]) && $_POST["action"] !== null){
+        if(csrf_valid($_POST)){
 
+        }else{
+            echo '{"status":"error", "type":"csrf"}';
+            exit;
+        }
     }else{
-        echo '{"status": "error"}';
+        echo '{"status": "error", "type":"no-action"}';
         exit;
     }
 ?>
