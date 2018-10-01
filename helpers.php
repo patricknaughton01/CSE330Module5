1dbea82729f2fa9cf0c44ce26b0df6ca06fdb6c0
Patrick Naughton <patricknaughton01@gmail.com>
2018-09-30 22:00:43 -0500

    Added connection to helpers.php

--- helpers.php
+++ helpers.php
@@ -3,4 +3,24 @@
     if(!isset($_SESSION["csrf"])){
         $_SESSION["csrf"] = bin2hex(openssl_random_pseudo_bytes(32));
     }
+
+
+    //******************Database**************************//
+    /*
+     * Connect to the news database and output an error if there is one.
+     *
+     * @return mysqli connection to the news database.
+     */
+    function connect(){
+        $uname_path = "/etc/330/calendar330/unrelated_info1";
+        $pword_path = "/etc/330/calendar330/unrelated_info2";
+        $uname = trim(file_get_contents($uname_path));
+        $pword = trim(file_get_contents($pword_path));
+        $mysqli = new mysqli('localhost', $uname, $pword, 'calendar');
+        if($mysqli->connect_errno){
+            printf("MySQL connection error: %s\n", $mysqli->connect_errno);
+            exit;
+        }
+        return $mysqli;
+    }
 ?>
